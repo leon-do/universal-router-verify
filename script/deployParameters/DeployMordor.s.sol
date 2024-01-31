38092a66b32c9f0d2230350d79f7dc90b14a5a9b
leon-do <leondo123@gmail.com>
2024-01-31 16:26:56 -0500

    mordor 0x65bB499C947723AF71609547F702E91D215197F0

---
+++ script/deployParameters/DeployMordor.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import {DeployUniversalRouter} from '../DeployUniversalRouter.s.sol';
+import {RouterParameters} from 'contracts/base/RouterImmutables.sol';
+
+contract DeployMordor is DeployUniversalRouter {
+    function setUp() public override {
+        params = RouterParameters({
+            permit2: 0x000000000022D473030F116dDEE9F6B43aC78BA3,
+            weth9: UNSUPPORTED_PROTOCOL,
+            seaportV1_5: UNSUPPORTED_PROTOCOL,
+            seaportV1_4: UNSUPPORTED_PROTOCOL,
+            openseaConduit: UNSUPPORTED_PROTOCOL,
+            nftxZap: UNSUPPORTED_PROTOCOL,
+            x2y2: UNSUPPORTED_PROTOCOL,
+            foundation: UNSUPPORTED_PROTOCOL,
+            sudoswap: UNSUPPORTED_PROTOCOL,
+            elementMarket: UNSUPPORTED_PROTOCOL,
+            nft20Zap: UNSUPPORTED_PROTOCOL,
+            cryptopunks: UNSUPPORTED_PROTOCOL,
+            looksRareV2: UNSUPPORTED_PROTOCOL,
+            routerRewardsDistributor: UNSUPPORTED_PROTOCOL,
+            looksRareRewardsDistributor: UNSUPPORTED_PROTOCOL,
+            looksRareToken: UNSUPPORTED_PROTOCOL,
+            v2Factory: UNSUPPORTED_PROTOCOL,
+            v3Factory: 0x6CDb6F10DD1BCb04c81A24481f5Dad01d0F0E533,
+            pairInitCodeHash: BYTES32_ZERO,
+            poolInitCodeHash: 0x7ea2da342810af3c5a9b47258f990aaac829fe1385a1398feb77d0126a85dbef
+        });
+
+        unsupported = 0x5302086A3a25d473aAbBd0356eFf8Dd811a4d89B;
+    }
+}
